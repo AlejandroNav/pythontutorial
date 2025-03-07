396b3bd8db07ae673ce06feb525c749c3618a8a8
Alex-Endevour <alejugger@gmail.com>
2025-03-07 10:41:38 -0600

    python

--- clase2.py
+++ clase2.py
@@ -1,1 +1,19 @@
 print("hola")
+
+#dictionary with 5 data for a student
+student = {
+    "name": "Luis",
+    "age": 20,
+    "gender": "male",
+    "height": 1.80,
+    "weight": 80
+}
+
+print(student)
+
+print(student["name"])
+
+for key in student:
+    print(key, " es el key y ", student[key], " es el value")
+
+    
